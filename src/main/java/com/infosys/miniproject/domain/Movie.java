db683a6d4db20a7c6bf5670200f9898a383cc931
Nathan Evans <nevans.dev@gmail.com>
2021-06-11 14:14:57 -0400

    NE: Added DB connection to H2.

--- src/main/java/com/infosys/miniproject/domain/Movie.java
+++ src/main/java/com/infosys/miniproject/domain/Movie.java
@@ -1,6 +1,16 @@
 package com.infosys.miniproject.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String title;
 	private String genre;
