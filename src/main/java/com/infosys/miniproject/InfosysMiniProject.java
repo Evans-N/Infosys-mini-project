e2abfe9ec89ef2eef0395bdb68cdf07ba0d9ef33
Nathan Evans <nevans.dev@gmail.com>
2021-06-11 09:50:28 -0400

    NE: Infosys mini project architecture.

---
+++ src/main/java/com/infosys/miniproject/InfosysMiniProject.java
@@ -0,0 +1,13 @@
+package com.infosys.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosysMiniProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfosysMiniProject.class, args);
+	}
+
+}
